f1c28020271b79d7e46660a5532671afb6f89211
Junsasaki0077 <ikasas0077@gmail.com>
2022-06-06 22:03:00 +0900

    cardTitleコンポーネント作成

--- src/components/task/TaskCard.jsx
+++ src/components/task/TaskCard.jsx
@@ -1,5 +1,16 @@
 import React from "react";
+import { TaskCardDeleteButton } from "./button/TaskCardDeleteButton";
+import { TaskAddInput } from "./input/TaskAddInput";
+import { Tasks } from "./Tasks";
+import { TaskCardTitle } from "./TaskCardTitle";
 
 export const TaskCard = () => {
-  return <div>TaskCard</div>;
+  return (
+    <div className="taskCard">
+      <TaskCardTitle />
+      <TaskCardDeleteButton />
+      <TaskAddInput />
+      <Tasks />
+    </div>
+  );
 };
---
+++ src/components/task/TaskCardTitle.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from "react";
+
+export const TaskCardTitle = () => {
+  const [isClick, setIsClick] = useState(false);
+  const [inputCardTitle, setInputCardTitle] = useState("Today");
+  const handleClick = () => {
+    setIsClick(true);
+  };
+  const handleChange = (e) => {
+    console.log(inputCardTitle);
+    setInputCardTitle(e.target.value);
+  };
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    setIsClick(false);
+  };
+  return (
+    <div onClick={handleClick}>
+      {isClick ? (
+        <form onSubmit={handleSubmit}>
+          <input type="text" onChange={handleChange} />
+        </form>
+      ) : (
+        <h3>{inputCardTitle}</h3>
+      )}
+    </div>
+  );
+};
