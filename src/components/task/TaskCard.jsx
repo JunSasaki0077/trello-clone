bb2212747d086a446ed03f6d829bc64be11e509d
Junsasaki0077 <ikasas0077@gmail.com>
2022-06-07 08:21:18 +0900

    入力情報をstateで管理

--- src/components/task/TaskCard.jsx
+++ src/components/task/TaskCard.jsx
@@ -1,15 +1,22 @@
-import React from "react";
+import React, { useState } from "react";
 import { TaskCardDeleteButton } from "./button/TaskCardDeleteButton";
 import { TaskAddInput } from "./input/TaskAddInput";
 import { Tasks } from "./Tasks";
 import { TaskCardTitle } from "./TaskCardTitle";
 
 export const TaskCard = () => {
+  const [inputText, setInputText] = useState("");
+  const [taskList, setTaskList] = useState([]);
   return (
     <div className="taskCard">
       <TaskCardTitle />
       <TaskCardDeleteButton />
-      <TaskAddInput />
+      <TaskAddInput
+        input={inputText}
+        setInputText={setInputText}
+        setTaskList={setTaskList}
+        taskList={taskList}
+      />
       <Tasks />
     </div>
   );
